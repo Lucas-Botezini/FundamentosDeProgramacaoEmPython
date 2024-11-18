2f1f8bc6bdfbb2b2ef70053ba894f52b5f901252
Lucas Botezini <114296072+Lucas-Botezini@users.noreply.github.com>
2024-11-18 20:01:09 -0300

    Exercicio 1 da Aula 1

---
+++ Aula 1/Ex1/dadosInformadosPeloUsuario.py
@@ -0,0 +1,12 @@
+# 1) Escreva um programa que leia o comprimento (cm), a largura (cm) e a altura (cm) de uma caixa retangular e calcule o seu volume (cm3), cuja fórmula é: Volume = Comprimento * Largura * Altura.
+# Teste o seu programa com as medidas abaixo:
+
+comprimento = float(input("Informe o comprimento: "))
+
+largura = float(input("Informe a largura: "))
+
+altura = float(input("Informe a altura: "))
+
+volume = comprimento * largura * altura
+
+print("Volume: ", volume)
