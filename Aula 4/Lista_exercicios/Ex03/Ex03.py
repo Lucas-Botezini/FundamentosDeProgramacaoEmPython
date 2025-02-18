bc044f3061a33aa2369bafaa8edf9983c531aa09
davi <daviaugustolira@gmail.com>
2025-02-17 21:02:35 -0300

    Exercicio 03 Loop

---
+++ Aula 4/Lista_exercicios/Ex03/Ex03.py
@@ -0,0 +1,29 @@
+## Faça um algoritmo que simule um caixa eletrônico para executar operações de saque, depósito e extrato
+
+listaOperacoes = []
+
+opcao = 1
+valor = 0
+while opcao != 0:
+    opcao = int(input("1 - Depósito\n2 - Saque\n3 - Extrato\n0 - Sair\nDigite: "))
+
+    match(opcao):
+        case 1:
+            deposito = float(input("Informe o valor de depósito: "))
+            valor += deposito
+            print("Valor atualizado: R$", valor)
+            listaOperacoes.append("Deposito : "+ str(deposito) +" - Valor: " + str(valor))
+        case 2:
+            saque = float(input("Informe o valor de depósito: "))
+            if saque > valor:
+                print("O valor do saque é maior que o valor disponível na conta\nDisponível R$", valor)
+            else:
+                valor -= saque
+                print("Valor atualizado: R$", valor)
+                listaOperacoes.append("Saque :"+ str(saque) +" - Valor: "+ str(valor))
+        case 3:
+            print("Valor disponível: R$", valor)
+            for index in listaOperacoes:
+                print(index)
+        case 0:
+            print("Saindo...Obrigado")
